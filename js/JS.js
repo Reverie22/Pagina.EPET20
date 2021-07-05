1d4a37b6733c6c26c5c82123c98bab5bee3e3224
Reverie22 <franco.campaas@gmail.com>
2021-07-05 15:11:40 -0300

    Añadiendo js y propbando comandos

---
+++ js/JS.js
@@ -0,0 +1,19 @@
+alert("hola");
+window.status
+function detectSO(){}
+var osName = "Desconocido";
+if(navigator.userAgent.indexOf("Win") !=1){
+    osName = "Windows";
+}
+if(navigator.userAgent.indexOf("Android") !=1){
+    osName = "Android";
+}
+    if (osName == "Windows"){
+        window.location
+    }
+    else{
+
+    
+        alert("No tenemos una web");
+        return false;
+    } 
