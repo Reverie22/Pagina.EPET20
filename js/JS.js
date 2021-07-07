cd9dc11373401b560c3d34fce595a6b26d2eae4a
Reverie22 <franco.campaas@gmail.com>
2021-07-07 14:51:47 -0300

    Probando js

--- js/JS.js
+++ js/JS.js
@@ -1,19 +1,46 @@
-alert("hola");
-window.status
-function detectSO(){}
-var osName = "Desconocido";
-if(navigator.userAgent.indexOf("Win") !=1){
-    osName = "Windows";
-}
-if(navigator.userAgent.indexOf("Android") !=1){
-    osName = "Android";
-}
-    if (osName == "Windows"){
-        window.location
-    }
-    else{
+alert("Hola! Bienvenido a Caulher.");
+var parrafo = document.createElement("p");
+var parrafo2 = document.createElement("p");
+var parrafo3 = document.createElement("p");
+var contenido = document.createTextNode("Pruebas");
+var parrafo1 = document.createTextNode("El sistema de alarma global permite cubrir toda la zona de interés, asegurando que todo elpersonal tenga aviso sonoro y lumínico ante cualquier evento de emergencia. Es un sistema autónomo, inteligente e inalámbrico, robusto, fácil de utilizar, de fácil instalación y libre de mantenimiento diario. El conjunto está formado por varias torres telescópicas con sirenas y balizas (B), autónomas y con enlace inalámbrico de baja potencia y largo alcance. Cada torre puede ser disparada localmente o en forma remota mediante pulsadores de comando. Además, se cuenta con unidades portátiles (P) para poder disparar la alarma global desde cualquier punto sin necesidad de acercarse a una torre. También, pueden sumarse “llaveros” para que la persona a cargo pueda controlar el sistema sin tener que acercarse hasta una torre o unidad portátil brindando mayor flexibilidad y rapidez a la hora de notificar un evento de emergencia. El sistema se complementa con unidades monitor (M), que se conectan a una PC/Notebook con sistema operativo Windows y mediante un Software de escritorio dedicado (S) se puede monitorear en tiempo real, el correcto funcionamiento del sistema; visualizando el estado de enlace inalámbrico, tensión de baterías, nivel de señal, evento de alarma, entre otros. Se pueden agregar tantas unidades Base Torre Autónomas (B), Unidades Portátiles (P) y Unidades Monitor (M) como sea necesario con el fin de cubrir toda la zona de interés, asegurando que todo el personal tenga aviso del evento de alarma, con la facilidad de poder disparar la alarma global desde cualquier punto.")
+parrafo.appendChild(parrafo1)
+document.body.appendChild(parrafo)
+parrafo3.appendChild(contenido);
+document.body.appendChild(contenido);
 
-    
-        alert("No tenemos una web");
-        return false;
-    } 
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+//window.status
+
+
+//(function detectSO(){}
+//var osName = "Desconocido";
+//if(navigator.userAgent.indexOf("Win") !=1){
+  //  osName = "Windows";
+//}
+//if(navigator.userAgent.indexOf("Android") !=1){
+  //  osName = "Android";
+//}
+  //  if (osName == "Windows"){
+    //    window.location
+    //}
+    //else{  
+      //   alert("No tenemos una web");
+    //} 
