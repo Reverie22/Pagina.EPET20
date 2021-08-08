c6c3d8b82cd9e937681cad102117462af18886e3
Franco Campas <franco.campaas@gmail.com>
2021-08-07 21:05:49 -0300

    aplicando animaciones al boton (en proceso)

--- js/JS.js
+++ js/JS.js
@@ -1,10 +1,14 @@
-alert ("Hola")
-var etiquetas = document.createElement("h1");
-var contenido = document.createTextNode("Caulher");
-var contenido2 = document.createTextNode("Servicios Integrales");
-etiquetas.appendChild.contenido;
 
+ addEventListener('DOMContentLoaded', () =>{
+ const boton1 = document.querySelector('#boton11');
+const irarriba = () => {
+  if (usuariohaceclick() > 0) {
+    scrollto(0,0);
+  }
 
+}
+ boton1.addEventListener('click', irarriba);
+})
 
 
 
@@ -13,24 +17,29 @@ etiquetas.appendChild.contenido;
         
 
 
+/*alert ("Hola");
+var etiquetas = document.createElement("h1");
+var contenido = document.createTextNode("Caulher");
+var contenido2 = document.createTextNode("Servicios Integrales");
+etiquetas.appendChild.contenido;
+
+*/
 
 
 
+/*window.status
 
-//window.status
 
-
-//(function detectSO(){}
-//var osName = "Desconocido";
-//if(navigator.userAgent.indexOf("Win") !=1){
-  //  osName = "Windows";
-//}
-//if(navigator.userAgent.indexOf("Android") !=1){
-  //  osName = "Android";
-//}
-  //  if (osName == "Windows"){
-    //    window.location
-    //}
-    //else{  
-      //   alert("No tenemos una web");
-    //} 
+(function detectSO(){}
+var osName = "Desconocido";
+if(navigator.userAgent.indexOf("Win") !=1){
+    osName = "Windows";
+}
+if(navigator.userAgent.indexOf("Android") !=1){
+    osName = "Android";
+}
+   if (osName == "Windows"){
+       window.location
+    }
+    else{  
+         alert("No tenemos una web")*/
